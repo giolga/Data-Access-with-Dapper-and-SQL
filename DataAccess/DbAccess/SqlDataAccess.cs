7b8702ca22fc4f8883e35a2794e35159f9464d56
giolga <giolga.kumi@gmail.com>
2024-08-09 16:33:00 +0400

    first commit

---
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess.DbAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+        public SqlDataAccess(IConfiguration config)
+        {
+            this._config = config;
+        }
+
+        public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure,
+                                                         U parameters,
+                                                         string connectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+            return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId)); //var connection
+
+            await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
